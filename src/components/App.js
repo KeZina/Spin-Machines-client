6a4b78d1af3102ed49096e856604fed201f926c9
Artyom K <kezina2036@gmail.com>
2020-03-03 11:43:32 +0300

    display done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Slot from './Slot';
 
 const App = () => {
   return (
     <>
+      <Slot />
     </>
-  );
+  )
 }
 
 export default App;
--- src/components/Slot.js
+++ src/components/Slot.js
@@ -2,8 +2,58 @@ import React from 'react';
 
 const Slot = () => {
     return (
-        <>
-        </>
+        <div id = 'slot'>
+            <div id = 'display'>
+                <div id = 'quests'>
+                    <div>
+                      <span>Do Spin:</span>
+                      <span>0/0</span>
+                    </div>
+                    <div>
+                        <span>Spent Money:</span>
+                        <span>0/0</span>
+                    </div>
+                    <div>
+                        <span>3 Symbol In Row:</span>
+                        <span>0/0</span>
+                    </div>
+                   <div>
+                       <span>Rare Sybmol:</span>
+                       <span>0/0</span>
+                   </div>
+                </div>
+                <div id = 'spin'>
+                    <div>
+                        <span>0</span>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                    <div>
+                        <span>0</span>
+                        <span>0</span>
+                        <span>0</span>
+                    </div>
+                </div>
+                <button id = 'shy-button'>
+                    Spin!
+                </button>
+            </div>
+        </div>
     )
 }
 
